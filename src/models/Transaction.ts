884c6596bbb7d51ba436380a53165ed7eb97cbca
Will Seagar <>
2019-09-25 15:16:05 +1200

    Add status to transaction

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -4,6 +4,7 @@ export interface Transaction {
     description: string;
     amount: number;
     currency: string;
+    status: string;
     creditDebitIndicator: string;
     type: string;
     categories: string[];
